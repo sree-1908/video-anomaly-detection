cd1ab0b43ca2f4e4ee46ab24b20f269cb298ba4e
sree-1908 <gayatrisreeja19@gmail.com>
2025-11-27 10:19:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -258,7 +258,7 @@ def Live9():
 
 
 
-
 if __name__ == "__main__":
-
-    app.run(debug=True, use_reloader=False)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
